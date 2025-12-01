e80220fe92b523c6ed3021131cb3e8b7aecbb05a
TorstenMeffert <torstenmeffert00@gmail.com>
2025-12-01 16:13:57 +0100

    yearly in out degrees

--- scripts/inout.py
+++ scripts/inout.py
@@ -11,6 +11,14 @@ def in_degrees(ds: xr.Dataset) -> np.ndarray:
         in_values[i] = sum(ds["network"].values[:, i])
     return in_values
 
+def yearly_in_degrees(scenario: str, year: int) -> np.ndarray:
+    """Calculate yearly in-degrees of nodes in the network."""
+    yearly_in_value = np.zeros(416)
+    for month in range(1,13):
+        filepath =[f"../data/scenario_ssp{scenario}_decade{year}_month{month:02d}.nc"]
+        ds = [xr.open_dataset(filepath)]
+        yearly_in_value[month] += in_degrees(ds[month])
+    return yearly_in_value 
 
 def out_degrees(ds: xr.Dataset) -> np.ndarray:
     """Calculate out-degrees of nodes in the network."""
@@ -18,3 +26,12 @@ def out_degrees(ds: xr.Dataset) -> np.ndarray:
     for i in range(ds.sizes["y"]):
         out_values[i] = sum(ds["network"].values[i])
     return out_values
+
+def yearly_out_degrees(scenario: str, year: int) -> np.ndarray:
+    """Calculate yearly out-degrees of nodes in the network."""
+    yearly_out_value = np.zeros(416)
+    for month in range(1,13):
+        filepath =[f"../data/scenario_ssp{scenario}_decade{year}_month{month:02d}.nc"]
+        ds = [xr.open_dataset(filepath)]
+        yearly_out_value[month] += out_degrees(ds[month])
+    return yearly_out_value 
--- scripts/plot_all.py
+++ scripts/plot_all.py
@@ -1,6 +1,6 @@
 """Script to plot a network of data tracks based on connection strengths."""
 from clustering import clustering_coefficients, feed_forward_loop
-from inout import in_degrees, out_degrees
+from inout import in_degrees, out_degrees, yearly_in_degrees, yearly_out_degrees
 
 import matplotlib.pyplot as plt
 
@@ -198,6 +198,75 @@ def plot_degrees(scenario: str, year: int, month: int) -> None:
     plt.close(fig)
 
 
+def plot_yearly_degrees(scenario: str, year: int) -> None:
+    """
+    Plot in-degrees and out-degrees from the dataset for an entire year.
+
+    Input:
+        Scenario (str): Number of the scenario
+        Year (int): Year number of dataset
+        
+    Output:
+        None, but plots saved to directory
+    """
+
+    filepath = f"../data/scenario_ssp245_decade2030_month12.nc"
+    ds = xr.open_dataset(filepath)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(22, 8))
+
+    fig.suptitle(
+        f"Network In- and Out-Degrees - Year: {year}", fontsize=16
+    )
+
+    # --- Plot 1: In-Degrees (on ax1) ---
+    sc1 = ax1.scatter(
+        x=ds["lon"],
+        y=ds["lat"],
+        c=yearly_in_degrees(scenario, year),
+        cmap="viridis",
+        s=300,
+        vmin=0,
+        vmax=1200,
+        marker="s",
+    )
+
+    ax1.set_title("In-Degrees (Connections To)")
+    ax1.set_xlabel("Longitude")
+    ax1.set_ylabel("Latitude")
+    ax2.set_xlim(-81, -48)
+    ax2.set_ylim(-22, 7)
+    # ax1.grid(True)
+    ax1.set_aspect("equal")
+    fig.colorbar(sc1, ax=ax1, label="Sum of Connections", shrink=0.78)
+
+    # --- Plot 2: Out-Degrees (on ax2) ---
+    sc2 = ax2.scatter(
+        x=ds["lon"],
+        y=ds["lat"],
+        c=yearly_out_degrees(ds),
+        cmap="viridis",
+        s=300,
+        vmin=0,
+        vmax=4200,
+        marker="s",
+    )
+    ax2.set_title("Out-Degrees (Connections From)")
+    ax2.set_xlabel("Longitude")
+    ax2.set_ylabel("Latitude")
+    ax2.set_xlim(-81, -48)
+    ax2.set_ylim(-22, 7)
+    # ax2.grid(True)
+    ax2.set_aspect("equal")
+    fig.colorbar(sc2, ax=ax2, label="Sum of Connections", shrink=0.78)
+
+    # plt.show()
+    plt.savefig(
+        f"../results/plots/InOut/Scenario{scenario}/yearly_degrees_{scenario}_{year}.png"
+    )
+    plt.close(fig)
+
+
 def plot_clustering(scenario: str, year: int, month: int) -> None:
     """Plot local clustering coefficient from the dataset."""
     filepath = f"../data/scenario_ssp{scenario}_decade{year}_month{month:02d}.nc"
@@ -275,4 +344,5 @@ if __name__ == "__main__":
     # plot_evaporation(scenario=scenario, year=year, month=month)
     # plot_network(scenario=scenario, year=year, month=month)
     # plot_degrees(scenario=scenario, year=year, month=month)
-    plot_clustering(scenario=scenario, year=year, month=month)
+    #plot_clustering(scenario=scenario, year=year, month=month)
+    plot_yearly_degrees(scenario=scenario, year=year)
