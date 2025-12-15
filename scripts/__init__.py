109ac2918999da3929680eb9f319c9f3dae5d7cd
Jakob Koepf <koepf@itp.uni-frankfurt.de>
2025-12-15 11:14:11 +0100

    Test Functions

---
+++ scripts/__init__.py
@@ -0,0 +1,4 @@
+"""Docstring for scripts package."""
+
+from .inout import in_degrees, out_degrees
+from .clustering import find_neighbourhood, clustering_coefficients, feed_forward_loop
--- tests/readme.txt
+++ tests/readme.txt
@@ -1,1 +1,4 @@
 tests: Where you put tests for your code. 
+
+Execute with (in root of project) python3 -m tests.test_functions
+     or with (in root of project) python3 -m pytest
