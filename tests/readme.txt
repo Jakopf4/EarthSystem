69b37441ab185b83dc18b26c8c770997b247f861
Jakob Koepf <koepf@itp.uni-frankfurt.de>
2026-01-20 17:43:46 +0100

    Cleaned up folder structure

--- tests/readme.txt
+++
@@ -1,4 +0,0 @@
-tests: Where you put tests for your code. 
-
-Execute with (in root of project) python3 -m tests.test_functions
-     or with (in root of project) python3 -m pytest
--- tests/test_function_to_test.py
+++
@@ -1,6 +0,0 @@
-"""Run tests for functions in the source folder."""
-
-
-def test_some_function():
-    """This function tests the some_function function."""
-    assert True is True
